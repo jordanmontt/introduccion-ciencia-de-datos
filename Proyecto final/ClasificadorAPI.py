2c1ea184c3c9333523504ba37d874aa385e69966
Jose Daniel Oropeza Soria <dany.orop@gmail.com>
2020-06-10 10:54:46 -0400

    [PROYECTO] Resolver problema de resultado entre el ETL y la API.

--- Proyecto final/ClasificadorAPI.py
+++ Proyecto final/ClasificadorAPI.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import json
 import pandas as pd
 import numpy as np
 import pickle
@@ -9,6 +10,8 @@ import os
 import re
 import nltk
 
+
+JSON_MIME_TYPE = 'application/json'
 app = Flask(__name__)
 
 
@@ -27,28 +30,27 @@ palabras_de_parada = cargar_objeto_de_archivo('palabras-parada.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    print("entro")
     solicitud_prediccion = request.get_json(force=True)
     critica = solicitud_prediccion['critica']
-
     # Limpiar el texto
     reg_limpiar_html = re.compile(
         '<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
     critica_sin_html = re.sub(reg_limpiar_html, '', critica)
     tokens = nltk.tokenize.casual.casual_tokenize(critica_sin_html, "english")
-    tokens_normalizados = [token.lower()
-                           for token in tokens if token not in palabras_de_parada]
+    tokens_normalizados = [token.lower() for token in tokens if token not in palabras_de_parada]
     critica_normalizada = " ".join(tokens_normalizados)
-
     # Obtener one hot vector
     one_hot_vector = np.zeros(len(vocabulario_problema), dtype=int)
+
     for token in critica_normalizada.split():
-        one_hot_vector[vocabulario_problema[token]] = 1
+        if (token in vocabulario_problema):
+            one_hot_vector[vocabulario_problema[token]] = 1
 
     # Predecir y retornar la respuesta
     matriz_np_critica = np.array(one_hot_vector).reshape(1,-1)
     respuesta = clasificador.predict(matriz_np_critica)
-    return {'respuesta': respuesta.tolist()}
+    content = json.dumps({'respuesta': respuesta[0]})
+    return content, 200, {'Content-Type': JSON_MIME_TYPE}
 
 
 if __name__ == '__main__':
