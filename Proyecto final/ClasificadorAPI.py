9c7ed5568ac9b0b1cfd01400cbb931f8e09ff7e2
Sebastian Jordan Montaño <sebastianjmt@hotmail.com>
2020-06-11 03:12:39 -0400

    [Proyecto] se refactorizó toda la lógica de limpiar y normalizar el texto a un archivo externo

--- Proyecto final/ClasificadorAPI.py
+++ Proyecto final/ClasificadorAPI.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from LimpiadorTexto import LimpiadorTexto
 import json
 import pandas as pd
 import numpy as np
@@ -33,21 +34,15 @@ def predict():
     solicitud_prediccion = request.get_json(force=True)
     critica = solicitud_prediccion['critica']
     # Limpiar el texto
-    reg_limpiar_html = re.compile(
-        '<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-    critica_sin_html = re.sub(reg_limpiar_html, '', critica)
-    tokens = nltk.tokenize.casual.casual_tokenize(critica_sin_html, "english")
-    tokens_normalizados = [token.lower() for token in tokens if token not in palabras_de_parada]
-    critica_normalizada = " ".join(tokens_normalizados)
+    limpiador = LimpiadorTexto(palabras_de_parada)
+    critica_normalizada = limpiador.normalizar(critica)
     # Obtener one hot vector
     one_hot_vector = np.zeros(len(vocabulario_problema), dtype=int)
-
     for token in critica_normalizada.split():
         if (token in vocabulario_problema):
             one_hot_vector[vocabulario_problema[token]] = 1
-
     # Predecir y retornar la respuesta
-    matriz_np_critica = np.array(one_hot_vector).reshape(1,-1)
+    matriz_np_critica = np.array(one_hot_vector).reshape(1, -1)
     respuesta = clasificador.predict(matriz_np_critica)
     content = json.dumps({'respuesta': respuesta[0]})
     return content, 200, {'Content-Type': JSON_MIME_TYPE}
